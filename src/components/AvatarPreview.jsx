cdddca92c069c68628d8f6a8fb2262f0b14a162d
CASHMIND X <theknightmentor@gmail.com>
2025-07-21 15:01:57 +0200

    Update AvatarPreview.jsx

--- src/components/AvatarPreview.jsx
+++ src/components/AvatarPreview.jsx
@@ -1,8 +1,22 @@
-const AvatarPreview = () => {
+// src/components/AvatarPreview.tsx
+import React from 'react';
+import { AvatarCreator } from './AvatarCreator';
+
+const AvatarPreview: React.FC = () => {
+  const handleUpgrade = () => {
+    console.log("Upgrade clicked");
+  };
+
+  const handleBack = () => {
+    console.log("Back clicked");
+  };
+
   return (
-    <div>Avatar Preview</div>
+    <AvatarCreator
+      onUpgrade={handleUpgrade}
+      onBack={handleBack}
+    />
   );
 };
 
 export default AvatarPreview;
-
